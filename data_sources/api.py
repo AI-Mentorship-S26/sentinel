6094f8463d841da6c6d40a2e05ac66f41f40075c
Roshan <roshankatta@gmail.com>
2026-04-26 17:04:05 -0500

    Resolved merge conflict

--- data_sources/api.py
+++ data_sources/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 import sys
 
@@ -8,6 +9,14 @@ sys.path.append(str(BASE_DIR / "models"))
 from predict_congestion import get_congestion_score
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
